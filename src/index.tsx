f7fbbf3bd7b763d8305c5f256d2b663a4fa7d2ac
Achmad Kurnianto <achmad.kurnianto@gmail.com>
2021-06-28 14:13:46 +0700

    feat(component): :sparkles: update `react-bus-esm` exported component and constants
    - add generics called `Events` to every methods and components.
    - make `Emitter` and `EventHandlerMap` generic to be optional

--- src/index.tsx
+++ src/index.tsx
@@ -1,60 +1,109 @@
 import React, {
-  FC,
   ReactNode,
+  ReactElement,
   createContext,
   useContext,
   useEffect,
   useState,
+  Context,
 } from 'react';
-import mitt, { Emitter, EventHandlerMap, WildcardHandler } from 'mitt';
+import mitt, {
+  Emitter as DefaultEmitter,
+  EventHandlerMap as DefaultEventHandlerMap,
+  Handler,
+  EventType,
+} from 'mitt';
 
-export { EventHandlerMap, Emitter } from 'mitt';
+export interface Emitter<
+  Events extends Record<EventType, unknown> = Record<EventType, unknown>
+> extends DefaultEmitter<Events> {}
 
-export const BusContext = createContext<Emitter | null>(null);
+export interface EventHandlerMap<
+  Events extends Record<EventType, unknown> = Record<EventType, unknown>
+> extends DefaultEventHandlerMap<Events> {}
 
-const { Provider: BusContextProvider } = BusContext;
+/**
+ *
+ *
+ * @export
+ * @template Events
+ * @param {EventHandlerMap<Events>} [options]
+ * @return {*}
+ */
+export function createBusContext<
+  Events extends Record<EventType, unknown> = Record<EventType, unknown>
+>(options?: EventHandlerMap<Events>) {
+  return createContext<Emitter<Events>>(mitt<Events>(options));
+}
+
+export const BusContext = createBusContext();
 
 /**
  * Return the event emitter.
  *
  * @export
+ * @template Events
+ * @param {*} [busContext=BusContext]
  * @return {*}
  */
-export function useBus(): Emitter {
-  return useContext(BusContext) as Emitter;
+export function useBus<
+  Events extends Record<EventType, unknown> = Record<EventType, unknown>
+>(busContext = BusContext) {
+  return useContext<Emitter<Events>>(busContext as Context<Emitter<Events>>);
 }
 
 /**
  * Attach an event listener to the bus while this component is mounted. Adds the listener after mount, and removes it before unmount.
  *
  * @export
+ * @template Events
  * @param {string} name
- * @param {WildcardHandler} fn
+ * @param {Handler<Events[keyof Events]>} fn
+ * @param {*} [busContext=BusContext]
  */
-export function useListener(name: string, fn: WildcardHandler) {
-  const bus = useBus();
-  const castedName = name as '*';
+export function useListener<
+  Events extends Record<EventType, unknown> = Record<EventType, unknown>
+>(name: string, fn: Handler<Events[keyof Events]>, busContext = BusContext) {
+  const bus = useBus<Events>(busContext);
   useEffect(() => {
-    bus.on(castedName, fn);
+    bus.on(name, fn);
     return () => {
-      bus.off(castedName, fn);
+      bus.off(name, fn);
     };
-  }, [bus, castedName, fn]);
+  }, [bus, name, fn]);
 }
 
-export interface ProviderProps {
+/**
+ *
+ *
+ * @export
+ * @interface ProviderProps
+ * @template Events
+ */
+export interface ProviderProps<
+  Events extends Record<EventType, unknown> = Record<EventType, unknown>
+> {
   children?: ReactNode;
-  mittOptions?: EventHandlerMap;
+  mittOptions?: EventHandlerMap<Events>;
+  busContext?: Context<Emitter<Events>>;
 }
 
 /**
  * Create an event emitter that will be available to all deeply nested child elements using the useBus() hook.
  *
  * @export
- * @param {ProviderProps} { children, mittOptions }
- * @return {*}
+ * @template Events
+ * @param {ProviderProps<Events>} { children, mittOptions, busContext = BusContext }
+ * @return {*}  {ReactElement<ProviderProps<Events>>}
  */
-export const Provider: FC<ProviderProps> = ({ children, mittOptions }) => {
-  const [bus] = useState(() => mitt(mittOptions));
-  return <BusContextProvider value={bus}>{children}</BusContextProvider>;
-};
+export function Provider<
+  Events extends Record<EventType, unknown> = Record<EventType, unknown>
+>({
+  children,
+  mittOptions,
+  busContext = BusContext as Context<Emitter<Events>>,
+}: ProviderProps<Events>): ReactElement<ProviderProps<Events>> {
+  const { Provider: BusProvider } = busContext;
+  const [bus] = useState(() => mitt<Events>(mittOptions));
+  return <BusProvider value={bus}>{children}</BusProvider>;
+}
